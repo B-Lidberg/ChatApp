dffee50b9197cac9280b7ec05445227b609bf7f1
B-Lidberg <76635047+B-Lidberg@users.noreply.github.com>
2021-08-03 11:08:24 -0400

    added darkmode option to a preview

--- app/src/main/java/com/lid/chatapp/presentation/components/chat_components/ChatBox..kt
+++ app/src/main/java/com/lid/chatapp/presentation/components/chat_components/ChatBox..kt
@@ -1,18 +1,16 @@
 package com.lid.chatapp.presentation.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.insets.ExperimentalAnimatedInsets
-import com.google.accompanist.insets.LocalWindowInsets
 import com.google.accompanist.insets.rememberImeNestedScrollConnection
-import com.google.accompanist.insets.rememberInsetsPaddingValues
 import com.lid.chatapp.data.model.ChatMessage
 
 @ExperimentalAnimatedInsets
@@ -60,7 +58,7 @@ fun ChatBoxAsBryanPreview() {
 }
 
 @ExperimentalAnimatedInsets
-@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun ChatBoxAsStrangerPreview() {
     val list = listOf<ChatMessage>(
