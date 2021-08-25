986163a7455d81b6f0ab6f02d8b492d9cbc40385
B-Lidberg <76635047+B-Lidberg@users.noreply.github.com>
2021-08-25 10:14:47 -0400

    add testing dependencies

--- app/src/androidTest/java/com/lid/chatapp/ChatAppInstrumentalTests.kt
+++ app/src/androidTest/java/com/lid/chatapp/ChatAppInstrumentalTests.kt
@@ -1,12 +1,22 @@
 package com.lid.chatapp
 
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.accompanist.insets.ExperimentalAnimatedInsets
+import com.lid.chatapp.presentation.screens.MainScreen
+import com.lid.chatapp.presentation.ui.theme.ChatAppTheme
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,7 +24,11 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ChatAppInstrumentalTests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
